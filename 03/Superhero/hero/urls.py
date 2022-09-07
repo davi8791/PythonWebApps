4c4730231e8ec386111c10afa09c8be1bdff4638
Mat <davi8791@bears.unco.edu>
2022-09-06 20:08:29 -0600

    Remove default superhero pages

--- 03/Superhero/hero/urls.py
+++ 03/Superhero/hero/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from .views import BlackWidow, HulkView, IndexView, IronManView, ScarletWitchView, SpiderManView, MantisView
+from .views import IndexView, ScarletWitchView, SpiderManView, MantisView
 
 urlpatterns = [
     path('', IndexView.as_view()),
-    path('hulk', HulkView.as_view()),
-    path('ironman', IronManView.as_view()),
-    path('blackwidow', BlackWidow.as_view()),
     path('scarletwitch', ScarletWitchView.as_view()),
     path('spiderman', SpiderManView.as_view()),
     path('mantis', MantisView.as_view()),
