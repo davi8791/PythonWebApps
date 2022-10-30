7c9c51b9335e36aed5b0c3a34f5fa8a72d8a42d1
Mat <davi8791@bears.unco.edu>
2022-10-30 16:26:51 -0600

    Add more tests for Project 8

--- Superhero/hero/tests.py
+++ Superhero/hero/tests.py
@@ -1,17 +1,26 @@
 from django.test import SimpleTestCase, TestCase
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-from .models import Hero, Author
+from .models import Hero, Author, Message
 from django.contrib.auth.models import User
 
+test_hero_data = dict(hero_name="test",
+                    real_name="test",
+                    about_1="test",
+                    about_2="test",
+                    about_source="test",
+                    quote="test",
+                    primary_rgb="test",
+                    strengths="test",
+                    weaknesses="test",
+                    image="test")
 
 def create_test_author(username=""):
     test_user = User.objects.create(username=username)
     return Author.objects.create(user=test_user)
 
-
 class HeroAppTest(SimpleTestCase):
-
     def test_django(self):
         self.assertTrue(True)
 
@@ -19,6 +28,7 @@ class HeroAppTest(SimpleTestCase):
 class DataTest(TestCase):
 
     def test_author_model(self):
+
         test_author1 = create_test_author("user1")
         test_author2 = create_test_author("user2")
         self.assertEqual(len(Author.objects.all()), 2)
@@ -32,32 +42,55 @@ class DataTest(TestCase):
     def test_hero_model(self):
 
         test_author = create_test_author()
-
         self.assertEqual(len(Hero.objects.all()), 0)
         hi = Hero.objects.create(author=test_author, hero_name='Title 1')
         Hero.objects.create(author=test_author, hero_name='Title 2')
         self.assertEqual(len(Hero.objects.all()), 2)
-
         a = Hero.objects.get(pk=2)
         self.assertEqual(a.hero_name, 'Title 2')
-
         a.hero_name = "New Title"
         a.save()
         self.assertEqual(a.hero_name, 'New Title')
-
         a.delete()
         self.assertEqual(len(Hero.objects.all()), 1)
 
 
-class HeroViewTest(TestCase):
+class ViewTest(TestCase):
+
+    def login(self):
+        user = get_user_model().objects.create_user(username="TESTUSER", password="TESTUSER")
+        self.assertEqual(self.client.login(username="TESTUSER", password="TESTUSER"), True)
+        return user
+
+class HeroViewTest(ViewTest):
+
     def test_hero_list_view(self):
-        self.assertEqual(reverse("hero_list"), "/hero/")
+        response = self.client.get(reverse("hero_list"))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_hero_detail_view(self):
+        user = self.login()
+        author = Author.objects.create(user=user)
+        Hero.objects.create(author=author, hero_name='title')
+        response = self.client.get(reverse("hero_detail", args='1'))
+        self.assertEqual(response.status_code, 200)
 
     def test_hero_add_view(self):
+        self.login()
+        self.client.post(reverse("hero_add"), test_hero_data)
+        self.assertEqual(len(Hero.objects.all()), 1)
+    
+    def test_hero_edit_view(self):
+        user = self.login()
+        author = Author.objects.create(user=user)
+        Hero.objects.create(author=author, hero_name='title')
+        self.client.post(reverse("hero_edit", args='1'), test_hero_data)
+        self.assertEqual(Hero.objects.all()[0].hero_name, "test")
 
-        test_author = create_test_author()
+    def test_hero_delete_view(self):
+        user = self.login()
+        author = Author.objects.create(user=user)
+        Hero.objects.create(author=author, hero_name='title')
+        self.client.post(reverse("hero_delete", args='1'))
+        self.assertEqual(len(Hero.objects.all()), 0)
 
-        a = dict(author=test_author, hero_name='T 1')
-        response = self.client.post(reverse("hero_add"), a)
-        print(response)
-        self.assertEqual(response.status_code, 302)
